40aa9c6014fe9cad2fe789b77841d0981cc7af11
Burnos-T <burnos.tv.mail@gmail.com>
2022-09-09 15:06:55 +0300

    Ex4: add test Get request

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
+    public void testEx3(){
         System.out.println("Hello from Tanya Burnos");
+    }
+
+    @Test
+    public void testEx4(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
